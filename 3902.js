b309ba44ec1c0eedd80e6afcf12d0eb58521c962
Seva <seva213415@inbox.ru>
2017-08-15 14:13:26 +0300

    with solution

--- 3902.js
+++ 3902.js
@@ -2,3 +2,23 @@
 // sum(1);       // => 1
 // sum(1)(2);    // => 3
 // sum(1)(2)(3); // => 6
+
+
+function sum(a) {
+var currentSum = a;
+
+function f(b) {
+    currentSum += b;
+    return f;
+}
+
+f.toString = function() {
+    return currentSum;
+};
+
+return f;
+}
+
+console.log( sum(1).toString() );
+console.log( sum(1)(2).toString() );
+console.log( sum(1)(2)(3).toString() );
